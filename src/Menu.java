6f293cb950c4b53262bab33afbfa10a92d51d056
Cheng Kevin <chengk.pro@gmail.com>
2021-02-19 18:46:51 +0100

    start menu

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Menu {
+
+    public static void mainFunction() {
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("|                          Bienvenue dans El-Presidente !                            |");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("\nRègles du jeu :\n ");
+        System.out.println("Vous incarnerez un dirigeant et serrez à la tête de votre nation.");
+        System.out.println("Le but étant d'être le meilleur Presidente ! Et pour cela vous allez devoir survivre \n" +
+                            "à un maximum d'évènements divers et variés. \n");
+    }
+}
