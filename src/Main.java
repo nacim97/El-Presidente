f6287abe5a15ba41157b01e38e7a9eda871dcb89
NoriTN <trannguyen.nori@outlook.com>
2021-02-21 23:22:12 +0100

    Final push

--- src/Main.java
+++ src/Main.java
@@ -23,21 +23,5 @@ public class Main {
                 }
 
             }
-        }
-        Factions commmmuniste = new Factions("Commmmuniste",50, 15, 0);
-        Factions liberaux = new Factions("Libéraux",50, 15, 0);
-        Factions religieux = new Factions("Religieux",50, 15,0);
-        Factions militariste = new Factions("Militariste",50, 15,0);
-        Factions ecologiste = new Factions("Ecologiste",50, 15,0);
-        Factions nationaliste = new Factions("Nationaliste",50, 15,0);
-        Factions loyaliste = new Factions("Loyaliste",100, 15,0);
-
-        Actions goldCapitaliste = new Actions("Pot de vin : Capitaliste",15,10,0,0,0,0,0,0,0,0);
-        Actions goldCommuniste = new Actions("Pot de vin : Communiste",15,0,10,0,0,0,0,0,0,0);
-        Actions goldLiberaux = new Actions("Pot de vin : Liberaux",15,0,0,10,0,0,0,0,0,0);
-        Actions goldReligieux = new Actions("Pot de vin : Religieux",15,0,0,0,10,0,0,0,0,0);
-        Actions goldMilitariste = new Actions("Pot de vin : Militariste",15,0,0,0,0,10,0,0,0,0);
-        Actions goldEcologiste = new Actions("Pot de vin : Ecologiste",15,0,0,0,0,0,10,0,0,0);
-        Actions goldNationaliste = new Actions("Pot de vin : Nationaliste",15,0,0,0,0,0,0,10,0,0);
-        Actions foodMarket = new Actions ("Acheter une unité de nourriture",8,0,0,0,0,0,0,0,0,1);
     }
+}
